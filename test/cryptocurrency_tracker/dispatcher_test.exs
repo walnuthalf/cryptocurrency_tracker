37c79fa4d72d9108fe2fd235ee29e06b57ad70ce
mk <mk@example.com>
2018-02-26 16:28:08 +1100

    added tests for Dispatcher

---
+++ test/cryptocurrency_tracker/dispatcher_test.exs
@@ -0,0 +1,30 @@
+defmodule CryptocurrencyTracker.DispatcherTest do
+  alias CryptocurrencyTracker.{Currencies, Dispatcher}
+  alias CryptocurrencyTrackerWeb.TrackerChannel
+  use CryptocurrencyTrackerWeb.ChannelCase
+
+  setup do 
+    # :ok = Ecto.Adapters.SQL.Sandbox.checkout(CryptocurrencyTracker.Repo)
+    # Ecto.Adapters.SQL.Sandbox.mode(CryptocurrencyTracker.Repo, {:shared, self()})
+    {:ok, dispatcher} = GenServer.start_link(Dispatcher, [name: DBChannelTestDispatcher]) 
+    {:ok, _, socket} =
+      socket("user_id", %{some: :assign})
+      |> subscribe_and_join(TrackerChannel, "tracker:lobby")
+    {:ok, socket: socket, dispatcher: dispatcher}
+  end
+
+  test "dispatcher saves to DB and broadcasts", %{dispatcher: dispatcher} do
+    rate_datas = [{"BTC", {:ok, 1111.1}}, 
+                  {"ETH", {:ok, 22.2}}, 
+                  {"BCH", {:ok, 333.21}}] 
+    symbol_to_rate = Enum.map(rate_datas, fn {symbol, {:ok, rate}} ->
+      {symbol, rate}
+    end) |> Map.new 
+    GenServer.call(dispatcher, {:new_rate_datas, rate_datas})  
+    time_now = DateTime.utc_now()
+    db_symbol_to_rate = Currencies.get_symbol_to_rate_at_time(time_now) 
+    assert Map.equal?(symbol_to_rate, db_symbol_to_rate)
+    assert_broadcast "new_rates", broadcasted_symbol_to_rate 
+    assert Map.equal?(symbol_to_rate, broadcasted_symbol_to_rate)
+  end
+end
