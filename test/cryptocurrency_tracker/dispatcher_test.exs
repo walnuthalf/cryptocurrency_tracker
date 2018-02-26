3acf3044b7ab2db7daf85e66a40095def1da2da8
mk <mk@example.com>
2018-02-26 19:14:27 +1100

    simplified dispatcher test

--- test/cryptocurrency_tracker/dispatcher_test.exs
+++ test/cryptocurrency_tracker/dispatcher_test.exs
@@ -1,5 +1,6 @@
 defmodule CryptocurrencyTracker.DispatcherTest do
-  alias CryptocurrencyTracker.{Currencies, Dispatcher}
+  alias CryptocurrencyTracker.{Repo, Dispatcher}
+  alias CryptocurrencyTracker.Currencies.RateInfo
   alias CryptocurrencyTrackerWeb.TrackerChannel
   use CryptocurrencyTrackerWeb.ChannelCase
 
@@ -13,6 +14,12 @@ defmodule CryptocurrencyTracker.DispatcherTest do
     {:ok, socket: socket, dispatcher: dispatcher}
   end
 
+  def convert_rate_infos(rate_infos) do
+    rate_infos |> Enum.map(fn rate_info -> 
+      {rate_info.symbol, rate_info.rate}
+    end) |> Map.new 
+  end
+
   test "dispatcher saves to DB and broadcasts", %{dispatcher: dispatcher} do
     rate_datas = [{"BTC", {:ok, 1111.1}}, 
                   {"ETH", {:ok, 22.2}}, 
@@ -21,8 +28,7 @@ defmodule CryptocurrencyTracker.DispatcherTest do
       {symbol, rate}
     end) |> Map.new 
     GenServer.call(dispatcher, {:new_rate_datas, rate_datas})  
-    time_now = DateTime.utc_now()
-    db_symbol_to_rate = Currencies.get_symbol_to_rate_at_time(time_now) 
+    db_symbol_to_rate = Repo.all(RateInfo) |> convert_rate_infos
     assert Map.equal?(symbol_to_rate, db_symbol_to_rate)
     assert_broadcast "new_rates", broadcasted_symbol_to_rate 
     assert Map.equal?(symbol_to_rate, broadcasted_symbol_to_rate)
